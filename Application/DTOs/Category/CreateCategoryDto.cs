dab175e26cea208379131901a6740da8c7ef939e
Ali Ahsan <aliahsan8751@gmail.com>
2026-02-26 18:47:46 +0500

    Added new controller

---
+++ Application/DTOs/Category/CreateCategoryDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExamDynamics.API.Application.DTOs.Category
+{
+    public class CreateCategoryDto
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(300)]
+        public string Description { get; set; } = string.Empty;
+    }
+}
---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExamDynamics.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController : ControllerBase
+    {
+       
+        
+    }
+}
