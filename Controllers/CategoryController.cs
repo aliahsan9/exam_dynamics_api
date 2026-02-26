cfe30053e24d8ea6fd20c37d6c3162f5dfc858bc
Ali Ahsan <aliahsan8751@gmail.com>
2026-02-26 20:36:43 +0500

    Added new Dto

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExamDynamics.API.Application.DTOs.Category;
+using ExamDynamics.API.Domain.Entities;
+using ExamDynamics.API.Infrastructure.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ExamDynamics.API.Controllers
 {
@@ -6,7 +11,71 @@ namespace ExamDynamics.API.Controllers
     [Route("api/[controller]")]
     public class CategoryController : ControllerBase
     {
-       
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // Get: api/category
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _context.Categories
+                .Select(c => new CategoryDto
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                    Description = c.Description
+                })
+                .ToListAsync();
+
+            return Ok(categories);
+        }
+
+        // Post api/category
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateCategoryDto dto)
+        {
+            var category = new Category
+            {
+                Name = dto.Name,
+                Description = dto.Description
+            };
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+
+            return Ok(category);
+        }
+        // PUT: api/category/1
         
+        [Authorize(Roles = "Admin")]
+        [HttpPut("id")]
+        public async Task<IActionResult> Update(int id, CreateCategoryDto dto)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+                return NotFound();
+            category.Name = dto.Name;
+            category.Description = dto.Description;
+
+            await _context.SaveChangesAsync();
+            return Ok(category);
+        }
+        // Delete: api/category/1
+        [Authorize(Roles = "Admin")]
+        [HttpDelete("id")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+
+            if (category == null)
+                return NotFound();
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+            return Ok("Category deleted");
+        }
     }
 }
+
